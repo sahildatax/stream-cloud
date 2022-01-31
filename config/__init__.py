347bc03d04194e91c432ff7b400448bc959c69b3
sahildatax <84317311+sahildatax@users.noreply.github.com>
2022-01-31 18:19:34 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = 1234
-    API_HASH = "21ab7cb0a453b5e60016dc7bbeb701cb"
-    CHANNEL = "-1001249461809"
+    API_ID = 8763712
+    API_HASH = "835d27216f117e22a5c192b89a4ce457"
+    CHANNEL = "-1001757403524"
     CHANNEL_USERNAME = "sahil_nolia"
-    TOKEN = "token"
+    TOKEN = "5151415399:AAFpNVs6Jx_5j6bDp2BJW4ZtOc8QFyz-cGk"
     DOMAIN  = os.getenv("domain","https://streamboty.nolia.repl.co")
