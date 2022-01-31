8dabd7821daf8a13833be90ea01a12c622bd34b7
sahildatax <84317311+sahildatax@users.noreply.github.com>
2022-01-31 18:56:24 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ import os
 class Config:
     API_ID = 8763712
     API_HASH = "835d27216f117e22a5c192b89a4ce457"
-    CHANNEL = "-1001757403524"
+    CHANNEL = eval("-1001757403524")
     CHANNEL_USERNAME = "sahil_nolia"
     TOKEN = "5151415399:AAEFBBOnPSaWVqpABEolz3SeKIfPjg-dt8Y"
     DOMAIN  = "https://stream-cloud-production-abb8.up.railway.app"
